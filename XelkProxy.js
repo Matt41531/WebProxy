574cb3bddf1012970c8b665a9b992fa62c5501fe
Matthew Rife <mari242@cs.uky.edu>
2019-03-19 13:36:45 -0400

    Server runs hello world

--- XelkProxy.js
+++ XelkProxy.js
@@ -1,10 +1,17 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
-}).listen(8181);
+const hostname = 'iris.cs.uky.edu';
+const port = 3000;
+const server = http.createServer(geturl);
+function geturl(req,res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write('Hello World!');
+	res.end();
+}
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 
 
 
