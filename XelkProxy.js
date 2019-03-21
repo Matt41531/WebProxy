3f4337ea4558e8850f11349aafef555ce9121a59
Matthew Rife <mari242@cs.uky.edu>
2019-03-21 13:42:48 -0400

    Added RegEx for LOCALFILE

--- XelkProxy.js
+++ XelkProxy.js
@@ -1,16 +1,34 @@
 var http = require('http');
+var XelkReq = require('/homes/paul/HTML/CS316/P3_Req.js');
 
 const hostname = 'iris.cs.uky.edu';
-const port = 3000;
+const port = 3300;
 const server = http.createServer(geturl);
+
 function geturl(req,res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write('Hello World!');
 	res.end();
+	console.log(req.url);
+	checkUrl(req.url);
+
 }
+
+function checkUrl(url) {
+	var localfileRegEx = /^(\/LOCALFILE\/)([a-zA-Z0-9\-\_\~])*(\/[a-zA-z0-9\-\_\~]*)*(\.)([a-zA-Z0-9]*)$/;
+	if(localfileRegEx.test(url)) {
+		console.log("Accepted");
+	}
+	else {
+		console.log("Rejected");
+	}
+}
+
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
 
 
 
