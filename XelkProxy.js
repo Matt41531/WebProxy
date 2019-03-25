60e5d7f8fd1a25ca9e68d392b81afa399a3d5be1
Matthew Rife <mari242@cs.uky.edu>
2019-03-25 19:10:53 -0400

    Added getHeaderType and serveFile

--- XelkProxy.js
+++ XelkProxy.js
@@ -11,11 +11,11 @@ function geturl(req,res) {
 	res.write('Hello World!');
 	res.end();
 	console.log(req.url);
-	checkUrl(req.url);
+	checkUrl(req.url, res);
 
 }
 
-function checkUrl(url) {
+function checkUrl(url,res) {
 	var accepted = false;
 	var localFileRegEx = "^(\/LOCALFILE\/)";
 	var hostRegEx = "(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)+([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])";
@@ -56,7 +56,11 @@ function checkUrl(url) {
 		}
 		else if(localFileFinalRegEx.test(url)) {
 			console.log("Local file");
-			checkExtension(url);
+			var fileExtension = checkExtension(url);
+			if(fileExtension) {
+				var headerType = getHeaderType(fileExtension);
+				serveFile(url,res,headerType);
+			}
 		}
 		else {
 			console.log("Local Exec");
@@ -71,23 +75,66 @@ function checkExtension(url) {
 	var validExtensions = XelkReq.extAllowed();
 	for(var i = 0; i<validExtensions.length; i++) {
 		var extensionToCheck = validExtensions[i][0];
-		extensionToCheck = new RegExp(extensionToCheck + "$");
-		if(extensionToCheck.test(url)){
+		extensionToCheckRegEx = new RegExp(extensionToCheck + "$");
+		if(extensionToCheckRegEx.test(url)){
 			extensionIsValid = true;
+			var finalExtension = extensionToCheck;
 		}
 	}
 
 	if(extensionIsValid) {
 		console.log("Valid extension");
+		
 	}
 	else {
 		console.log("Invalid extension");
 	}
+
+	return finalExtension;
+
+}
+
+function getHeaderType(fileExtension) {
+	var extAllowed = XelkReq.extAllowed();
+	var headerType;
+	var finalIndex0, finalIndex1;
+	for(var i = 0; i< extAllowed.length; i++) {
+		var subArray = extAllowed[i];
+  		var extIndex = subArray.indexOf(fileExtension);
+		if(extIndex != -1) {
+			finalIndex0 = i;
+			finalIndex1 = extIndex + 1; //HeaderType is the element after the extension
+		}
+	}	
+	if(finalIndex0) {
+		headerType = extAllowed[finalIndex0][finalIndex1];
+	}
+	else {
+		headerType = 'text/plain';
+	}
+
+	return headerType;
 }
 
 function generatePort() {
 	var port = Math.floor(Math.random() * (+XelkReq.UpperPort() - +XelkReq.LowerPort()) + +XelkReq.LowerPort());
 	return port;
+}
+
+function serveFile(url,res,headerType) {
+	const fs = require('fs');
+	var fileDir = XelkReq.fileDir();
+	var extAllowed = XelkReq.extAllowed();
+	res.writeHead(200, {'Content-Type': headerType});
+	//Remove LOCALFILE from filepath
+	url = url.split("/");
+	url.splice(1,1);
+	url = url.join("/");
+	fs.readFile(fileDir + url, (err, data) => {
+  		if (err) throw err;
+  		console.log(data);
+	});
+	console.log(fileDir+url);
 }
 
 server.listen(port, hostname, () => {
